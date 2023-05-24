47081f0cedb67294cfe6b4df18e6759fad8e11ef
tama-yo <tama.yo.996@ctc-g.co.jp>
2023-05-24 16:39:09 +0900

    update

--- src/main/java/com/example/spring/firebase/service/MyFirebaseService.java
+++ src/main/java/com/example/spring/firebase/service/MyFirebaseService.java
@@ -14,13 +14,16 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.messaging.AndroidConfig;
 import com.google.firebase.messaging.AndroidConfig.Priority;
 import com.google.firebase.messaging.AndroidNotification;
+import com.google.firebase.messaging.ApnsConfig;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
 
 @Service
 public class MyFirebaseService {
@@ -47,7 +50,7 @@ public class MyFirebaseService {
 
 		// 01:iPhoneApp. 02:AndroidApp
 		if ("01".equals(pushRequestDto.getTerminalType())) {
-
+			sendIphoneMessage(pushRequestDto, appInstanceTokenList);
 		} else if ("02".equals(pushRequestDto.getTerminalType())) {
 			this.sendAndroidMessage(pushRequestDto, appInstanceTokenList);
 		}
@@ -107,9 +110,48 @@ public class MyFirebaseService {
 	 * @param appInstanceTokenList
 	 */
 	private void sendIphoneMessage(PushRequestDto pushRequestDto, List<String> appInstanceTokenList) {
-		// TODO
-		//		Aps aps = Aps.builder().setContentAvailable(true);
-		//		ApnsConfig apnsConfig = ApnsConfig.builder().setAps(aps);
+
+		Map<String, Object> dataMap = new HashMap<String, Object>();
+		dataMap.put("type", "targeted");
+		dataMap.put("content_type", "text/plain");
+		dataMap.put("platform", pushRequestDto.getTerminalType());
+		dataMap.put("content", pushRequestDto.getContent());
+		
+		Notification notification = Notification.builder() //
+				.setTitle(pushRequestDto.getTitle()) //
+				.setBody(pushRequestDto.getPopupText()) //
+				.build();
+
+//		ApsAlert apsAlert = ApsAlert.builder()
+//				.setTitle(pushRequestDto.getTitle()) //
+//				.setBody(pushRequestDto.getPopupText()) //
+//				.build();
+//
+//		Aps aps = Aps.builder() //
+//				.setContentAvailable(true) //
+//				.setAlert(apsAlert)//
+//				.build();
+		
+		ApnsConfig apnsConfig = ApnsConfig.builder() //
+				//.setAps(aps) //
+				.putAllCustomData(dataMap) //
+				.build();
+		
+		appInstanceTokenList.stream().distinct().forEach(token -> {
+			Message message = Message.builder()
+					.setNotification(notification)
+					.setToken(token)
+					.setApnsConfig(apnsConfig)
+					.build();
+
+			try {
+				this.firebaseMessaging.send(message);
+			} catch (FirebaseMessagingException e) {
+				e.printStackTrace();
+			}
+			return;
+
+		});
 
 	}
 
@@ -143,14 +185,18 @@ public class MyFirebaseService {
 	private void saveNotification(PushRequestDto pushRequestDto) {
 		DatabaseReference ref = firebaseDatabase.getReference().child("push-request");
 
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-ddHHmmss");
-//		String notificationTime = LocalDateTime.now().format(formatter);
-		
+		//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-ddHHmmss");
+		//		String notificationTime = LocalDateTime.now().format(formatter);
+
 		String key = ref.push().getKey();
 		ref.child(key).setValueAsync(pushRequestDto);
-		
-//		String child = String.valueOf(pushRequestDto.getPushRequestSeq());
-//		ref.child(child).setValueAsync(pushRequestDto);
+
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("timestamp", ServerValue.TIMESTAMP);
+		ref.child(key).updateChildrenAsync(map);
+
+		//		String child = String.valueOf(pushRequestDto.getPushRequestSeq());
+		//		ref.child(child).setValueAsync(pushRequestDto);
 
 	}
 
